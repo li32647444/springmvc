6a7692a3dc39496f09ff8dbebb364ab6976ce55a
admin <Administrator@PC201604292146>
2016-10-20 16:45:17 +0800

    `

---
+++ src/main/java/com/lb/springmvc/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.lb.springmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value = "toLogin")
+	public String toIndex(HttpServletRequest request,HttpServletResponse response){
+		return "";
+	}
+	
+}
