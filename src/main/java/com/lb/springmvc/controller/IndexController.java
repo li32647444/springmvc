077e377dd3e40c829d4705679f2dedc1ddbfe88c
admin <Administrator@PC201604292146>
2016-10-21 15:28:18 +0800

    123

--- src/main/java/com/lb/springmvc/controller/IndexController.java
+++ src/main/java/com/lb/springmvc/controller/IndexController.java
@@ -5,12 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 	
-	@RequestMapping(value = "toIndex")
+	@RequestMapping(value = "/",method = RequestMethod.GET)
+	public String toIndex_First(HttpServletRequest request,HttpServletResponse response){
+		return "index";
+	}
+	
+	@RequestMapping(value = "/toIndex",method = RequestMethod.GET)
 	public String toIndex(HttpServletRequest request,HttpServletResponse response){
+		System.out.println("IndexController.toIndex()==>");
 		return "index";
 	}
 	
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+    String path = request.getContextPath();
+    String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Index.jsp</title>
+	<!-- base需要放到head中 -->    
+    <base href="<%=basePath%>">   
+</head>
+<body>
+	<center>
+		<img alt="" src="images/qq_login.png" />
+	</center>
+</body>
+</html>
