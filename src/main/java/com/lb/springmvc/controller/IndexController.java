b723d8c62a3df37b4c85b1bd7c4639a114be42d0
admin <Administrator@PC201604292146>
2016-10-21 17:24:04 +0800

    123

--- src/main/java/com/lb/springmvc/controller/IndexController.java
+++ src/main/java/com/lb/springmvc/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.lb.springmvc.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 	
+	// 添加slf4j日志实例对象
+	private final static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
 	@RequestMapping(value = {"/","toIndex"},method = RequestMethod.GET)
 	public String toIndex(HttpServletRequest request,HttpServletResponse response){
-		System.out.println("IndexController.toIndex()==>");
+		logger.info("IndexController.toIndex()==>");
 		return "index";
 	}
 	
