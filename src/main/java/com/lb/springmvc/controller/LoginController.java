2f3bf7ca050413a6e863f885fd5a20518f16faa7
admin <Administrator@PC201604292146>
2016-10-24 18:26:12 +0800

    1024-hx

--- src/main/java/com/lb/springmvc/controller/LoginController.java
+++ src/main/java/com/lb/springmvc/controller/LoginController.java
@@ -11,16 +11,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-	
-		private final static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-		@RequestMapping(value = {"/","toLogin"},method = RequestMethod.GET)
-		public String toLogin(HttpServletRequest request,HttpServletResponse response){
+	private final static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	@RequestMapping(value = { "/", "toLogin" }, method = RequestMethod.GET)
+	public String toLogin(HttpServletRequest request, HttpServletResponse response) {
+		return "login";
+	}
+
+	@RequestMapping(value = { "login" }, method = RequestMethod.GET)
+	public String login(HttpServletRequest request, HttpServletResponse response) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if(username == null || "".equals(username)){
+			request.setAttribute("msg", "请输入用户名！");
+			return "login";
+		} else if(password == null || "".equals(password)){
+			request.setAttribute("msg", "请输入密码！");
 			return "login";
 		}
-		
-		@RequestMapping(value = {"Login"},method = RequestMethod.GET)
-		public String login(HttpServletRequest request,HttpServletResponse response){
-			return "index";
-		}
+		return "index";
+	}
+
 }
